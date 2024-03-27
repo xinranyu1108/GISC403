6ae1c3dc839ffe90f8979082410326924dee5f0a
xinranyu1108 <46943626+xinranyu1108@users.noreply.github.com>
2024-03-28 10:16:55 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -2,16 +2,9 @@
 // UPDATE THIS WITH A BETTER STARTING LATITUDE AND LONGITUDE AND ZOOM LEVEL
 
 
-const map = L.map('map').setView([-43.5299, 172.6333], 12);
+const map = L.map('map').setView([-43.5299, 172.6333], 11);
 L.control.scale().addTo(map);
-/*
-const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/gdmckenzie/cltyydmci01x001raftqa5iwo/draft/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2RtY2tlbnppZSIsImEiOiJjbHNtZjZmdXEwb2h4MmltdGJodXd1MmpyIn0.UrHBVKEcCwL1MBqo8k7bmA', {
-	maxZoom: 19,
-	tileSize: 512,
-    zoomOffset: -1,
-	attribution: '&copy; OpenStreetMap | MapBox - Projection: WGS84 Spherical Mercator'
-}).addTo(map);
-*/
+
 const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/gdmckenzie/cltyydmci01x001raftqa5iwo/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2RtY2tlbnppZSIsImEiOiJjbHNtZjZmdXEwb2h4MmltdGJodXd1MmpyIn0.UrHBVKEcCwL1MBqo8k7bmA', {
 	maxZoom: 19,
 	tileSize: 512,
@@ -19,24 +12,63 @@ const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/gdmckenzie/cltyydmci
 	attribution: '&copy; OpenStreetMap | MapBox - Projection: WGS84 Spherical Mercator'
 }).addTo(map);
 
+//Routing services
+L.Routing.control({
+    waypoints: [
+        L.latLng(-43.5036591272169, 172.59019528821776),
+        L.latLng(-43.4920710877524, 172.56538491867016),
+        L.latLng(-43.52249095465049, 172.58267360542007),
+        L.latLng(-43.53097572150117, 172.59642649682547),
+        L.latLng(-43.52996100784076, 172.63834875935058),
+
+    ],
+    lineOptions: {
+        styles: [{ color: '#0570b0', weight: 4 }]
+    },
+    routeWhileDragging: false,
+    geocoder: false,
+    show: false,
+    createMarker: function() {
+        return null;
+    },
+    fitSelectedRoutes: false
+}).addTo(map);
+
+L.Routing.control({
+    waypoints: [
+        L.latLng(-43.52996100784076, 172.63834875935058),
+        L.latLng(-43.58540912503021, 172.70752891400159)
+    ],
+    lineOptions: {
+        styles: [{ color: 'orange', weight: 4, dashArray: '10, 10' }]
+    },
+    routeWhileDragging: false,
+    geocoder: false,
+    show: false,
+    createMarker: function() {
+        return null;
+    },
+    fitSelectedRoutes: false
+
+}).addTo(map)
+
 // There are four markers below.  Each has a latitude and longitude.  Edit these to be your unique places of interest.
 // Also note that each has its own 'pop-up.'  Edit the text in each of these to say why each location is important to you.
 
-
 const marker1 = L.marker([-43.52996100784076, 172.63834875935058]).addTo(map)
-	.bindPopup('The Press: This is where I work and collaborate with my team.');
+	.bindPopup('The Press: This is where I work and collaborate with my team.', { autoPan: false });
 
 const marker2 = L.marker([-43.5036591272169, 172.59019528821776]).addTo(map)
-    .bindPopup('My home: the place where I live and relax.');
+    .bindPopup('My home: the place where I live and relax.', { autoPan: false });
 
 const marker3 = L.marker([-43.53097572150117, 172.59642649682547]).addTo(map)
-	.bindPopup('Riccarton Paknsave: I shop here for my daily groceries.');
+	.bindPopup('Riccarton Paknsave: I shop here for my daily groceries.', { autoPan: false });
 
 const marker4 = L.marker([-43.4920710877524, 172.56538491867016]).addTo(map)
-	.bindPopup('Burnside City Fitness: I exercise here regularly for fitness');
+	.bindPopup('Burnside City Fitness: I exercise here regularly for fitness', { autoPan: false });
 
 const marker5 = L.marker([-43.52249095465049, 172.58267360542007]).addTo(map)
-	.bindPopup('University of Canterbury: I study here, gaining knowledge and skills. ');
+	.bindPopup('University of Canterbury: I study here, gaining knowledge and skills. ', { autoPan: false });
 
 
 // These are the Territorial Authority Polygons that are being accessed from a 3rd party server.
@@ -50,10 +82,8 @@ var geojsonLayer = new L.GeoJSON.AJAX("https://raw.githubusercontent.com/gdmcken
 
 	});
 
+
 geojsonLayer.addTo(map);
-
-
-
 
 // This creates a polygon on your map with provided coordinates.  Edit this polygon to create your own polygon somewhere in New Zealand
 // Edit the style of this polygon (see: https://leafletjs.com/reference.html#path)
@@ -69,29 +99,13 @@ const polygon = L.polygon([
 		fillColor: 'orange',
 	}).addTo(map).bindPopup("Christchurch Gondola: My favorite hiking spot. I visit here occasionally for a good workout or to hang out with friends.")
 
-
-
-
 setTimeout(ReOrder, 1000);
 function ReOrder() {
 	geojsonLayer.bringToBack();
 	polygon.bringToFront();
-
-	var latlngs = [
-
-	[-43.5036591272169, 172.59019528821776],
-	[-43.4920710877524, 172.56538491867016],
-   [-43.52249095465049, 172.58267360542007],
-    [-43.53097572150117, 172.59642649682547],
-    [-43.52996100784076, 172.63834875935058]
-
-];
-    var latlngs2 = [[-43.52996100784076, 172.63834875935058],[-43.58540912503021, 172.70752891400159]]
-
-var polyline = L.polyline(latlngs, {color: '#0570b0'}).addTo(map);
-var polydashedline = L.polyline(latlngs2, {color: 'orange', weight: '3',  dashArray: '8, 8', dashOffset: '0'}).addTo(map);
-
 }
+
+
 
 
 // END OF DOCUMENT
